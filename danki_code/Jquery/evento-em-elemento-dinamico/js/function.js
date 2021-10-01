79389c06a302e119926ce8b18625600eb99fc580
Guilherme Souza <guilherme.amsouza@gmail.com>
2021-09-30 23:01:21 -0300

    att em estudos jquery

--- danki_code/Jquery/evento-em-elemento-dinamico/js/function.js
+++ danki_code/Jquery/evento-em-elemento-dinamico/js/function.js
@@ -1,7 +1,18 @@
 $(function(){
 
-    $('body').on('click', function(){
-        alert('Sim! Está funcionando')
-    })
-    $('body').html('<a href="">Meu link está funcionando?</a>')
+    // $('body').on('click', function(){
+    //     alert('Sim! Está funcionando')
+    // })
+    // $('body').html('<a href="">Meu link está funcionando?</a>')
+
+    var func = function(){
+        $(this).css('background-color','green').css('color','#fff')
+    }
+
+    $('input[name=nome]').keyup(func)
+
+    var func2 = function(){
+        $(this).css('background-color','red').css('color','#fff')
+    }
+    $('input[name=nome1]').keydown(func2)
 })
