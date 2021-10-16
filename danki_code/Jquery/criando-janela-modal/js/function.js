5916eed4d436f7dc39a48920c7b7fd5b08a01a7f
Guilherme Souza <guilherme.amsouza@gmail.com>
2021-10-15 21:06:22 -0300

    formulário com funções em jquery

--- danki_code/Jquery/criando-janela-modal/js/function.js
+++ danki_code/Jquery/criando-janela-modal/js/function.js
@@ -1,15 +1,38 @@
 $(function(){
 
     let back = $('.bg')
-    let bt = $('.btn')
+    let bt = $('.botao')
+    let close = $('#fechar')
+    let sended = $('#enviado')
+    let form = $('.container-form')
 
     abrirJanela()
+    fecharJanela()
+    enviado()
 
     function abrirJanela(){
         bt.click(function(){
             back.fadeIn()
         })
     }
+
+    function fecharJanela(){
+        close.click(function(){
+            back.fadeOut()
+        })
+        back.click(function(){
+            back.fadeOut()
+        })
+    }
+    function enviado(){
+        sended.click(function(){
+            alert('enviado!')
+        })
+    }
+
+    form.click(function(e){
+        e.stopPropagation()
+    })
 })
 
 
