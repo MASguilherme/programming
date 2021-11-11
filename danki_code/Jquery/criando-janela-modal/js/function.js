fe6b891f81d9a3039697ddeb175afa5a3e016ecc
Guilherme Souza <guilherme.amsouza@gmail.com>
2021-11-10 23:07:20 -0300

    completando validação do modal

--- danki_code/Jquery/criando-janela-modal/js/function.js
+++ danki_code/Jquery/criando-janela-modal/js/function.js
@@ -6,6 +6,22 @@ $(function(){
     let sended = $('#enviado')
     let form = $('.container-form')
 
+    // $(function(){
+    //     let frase = 'guilherme@gmail.com'
+    //     let encontrar = frase.match(/^(.*?)@(.*?)$/)
+    //     if(encontrar != null) {
+    //         console.log('Encontrei algo:')
+    //         console.log(encontrar)
+    //         console.log('detalhado:')
+    //         console.log(encontrar[1])
+    //         console.log(encontrar[2])
+
+    //     }else{
+    //         console.log('Não temos nada')
+    //     }
+    // })
+  
+
     abrirJanela()
     fecharJanela()
     enviado()
@@ -33,6 +49,40 @@ $(function(){
     form.click(function(e){
         e.stopPropagation()
     })
+
+
+    $('form#form1').submit(function(){
+
+        let nome = $('input[name=nome]').val()
+        let telefone = $('input[name=tel').val()
+        let email = $('input[nmae=email]').val()
+    
+        let amount = nome.split(' ').length
+        let splitStr = nome.split(' ')
+    
+        // Contando a quantidade de espações e respectivos valores
+        if(amount >= 2){
+            for(let i = 0; i < amount; i++){
+                if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)){
+                    alert('Nome completo foi cadastrado sem números')
+                }else{
+                    alert('Nome completo não pode conter números ou começar com letra minuscula')
+                    campoInvalido($('input[name=nome]'))
+                    return false
+                }
+            }
+        }else{
+            alert('Nome completo não foi cadastrado!')
+            campoInvalido($('input[name=nome]'))
+            return false
+        }
+    })
+    // el == elemento
+    function campoInvalido(el){
+        $('input[name=nome]').css('border','2px solid red')
+        $('input[name=nome]').data('invalido','true')
+    }
+
 })
 
 
