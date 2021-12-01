5811678e863202a10f15e3c48cddf8da5c8218d0
Guilherme Souza <guilherme.amsouza@gmail.com>
2021-12-01 16:53:50 -0300

    att

--- danki_code/Jquery/jsocial-exemplo/js/function.js
+++ danki_code/Jquery/jsocial-exemplo/js/function.js
@@ -1,28 +1,10 @@
 $(function(){
-
-    VerificarCliqueMenu()
-
-    function VerificarCliqueMenu(){
-        $('a').click(function(){
-            let href = $(this).attr('href')
-            $.ajax({
-                'beforeSend': function(){
-                    console.log(`Estamos chamando o beforeSend`)
-                },
-                'timeout':10000,
-                'url': href,
-                'error':function( jqXHR, textStatus, errorThrown){
-                    if(jqXHR.statusText == 'Not Found'){
-                        alert('página não encontrada')
-                    }
-                },
-                'success':function(data){
-                    $('#container').html(data)
-                }
-                
-            })
-            return false            
-        })
-    }
+    // plugin media
+    $('#media-social').jsSocials({
+        shares: ['twitter', 'facebook']
+    })
+    // plugin  mask
+    $('input[name=telefone]').mask('(99)9999-999')
+    $('input[name=input]')
 
 })
