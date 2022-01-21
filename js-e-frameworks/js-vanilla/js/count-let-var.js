9f64c10e82767bee10ad6595cfb2a1afc351a253
Guilherme Souza <guilherme.amsouza@gmail.com>
2022-01-20 22:38:49 -0300

    js vanilla

---
+++ js-e-frameworks/js-vanilla/js/count-let-var.js
@@ -0,0 +1,13 @@
+const nome = "guilherme"
+// variavel que n deixa mudar o valor a ser chamada novamente
+console.log(nome)
+
+var idade = 24
+console.log(idade)
+
+if(true){
+    // let só pode ser usado dentro da condição que é colocado.
+    let nome2 = "Saulo"
+    console.log(nome2)
+}
+console.log(nome2)
