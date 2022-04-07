6dba3adc27a1c2ffbaa9f8fa5bbd3fba99997990
Guilherme Souza <guilherme.amsouza@gmail.com>
2022-04-06 23:19:49 -0300

    dom js

--- DOM/Dicee Challenge - Starting Files/dicee.js
+++ DOM/Dicee Challenge - Starting Files/dicee.js
@@ -1,25 +1,91 @@
-var n = Math.floor(Math.random() * 6 + 1);
 
 var dicesOne = document.querySelector('#diceOne');
+var dicesTwo = document.querySelector('#diceTwo');
 function playDices(){
-    for(var i = 0; i <= 1000; i++){
+    var n = Math.floor(Math.random() * 6 + 1);
         if(n === 1){
-            dicesOne.innerHTML = `<img id="diceOne"class="img1" src="images/dice1.png">`
+            dicesOne.innerHTML = `<div class="dice" id="diceOne">
+            <p>Player 1</p>
+            <img class="img1" src="images/dice1.png" alt="dice">
+          </div>`;
+
         }
         if(n === 2){
-            dicesOne.innerHTML = `<img id="diceOne"class="img1" src="images/dice2.png">`
+            dicesOne.innerHTML = `<div class="dice" id="diceOne">
+        <p>Player 1</p>
+        <img class="img1" src="images/dice2.png" alt="dice">
+      </div>`;
         }
         if(n === 3){
-            dicesOne.innerHTML = `<img id="diceOne"class="img1" src="images/dice3.png">`
+            dicesOne.innerHTML = `<div class="dice" id="diceOne">
+        <p>Player 1</p>
+        <img class="img1" src="images/dice3.png" alt="dice">
+      </div>`;
         }
         if(n === 4){
-            dicesOne.innerHTML = `<img id="diceOne"class="img1" src="images/dice4.png">`
+            dicesOne.innerHTML = `<div class="dice" id="diceOne">
+        <p>Player 1</p>
+        <img class="img1" src="images/dice4.png" alt="dice">
+      </div>`;
         }
         if(n === 5){
-            dicesOne.innerHTML = `<img id="diceOne"class="img1" src="images/dice5.png">`
+            dicesOne.innerHTML = `<div class="dice" id="diceOne">
+        <p>Player 1</p>
+        <img class="img1" src="images/dice5.png" alt="dice">
+      </div>`;
         }
         if(n === 6){
-            dicesOne.innerHTML = `<img id="diceOne"class="img1" src="images/dice6.png">`
+            dicesOne.innerHTML = `<div class="dice" id="diceOne">
+        <p>Player 1</p>
+        <img class="img1" src="images/dice6.png" alt="dice">
+      </div>`;
         }
+    var n2 = Math.floor(Math.random() * 6 + 1);
+    if(n2 === 1){
+        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
+        <p>Player 2</p>
+        <img class="img2" src="images/dice1.png" alt="dice">
+      </div>`;
+    }
+    if(n2 === 2){
+        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
+        <p>Player 2</p>
+        <img class="img2" src="images/dice2.png" alt="dice">
+      </div>`;
+    }
+    if(n2 === 3){
+        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
+        <p>Player 2</p>
+        <img class="img2" src="images/dice3.png" alt="dice">
+      </div>`;
+    }
+    if(n2 === 4){
+        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
+        <p>Player 2</p>
+        <img class="img2" src="images/dice4.png" alt="dice">
+      </div>`;
+    }
+    if(n2 === 5){
+        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
+        <p>Player 2</p>
+        <img class="img2" src="images/dice5.png" alt="dice">
+      </div>`;
+    }
+    if(n2 === 6){
+        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
+        <p>Player 2</p>
+        <img class="img2" src="images/dice6.png" alt="dice">
+      </div>`;
+    }
+
+    var titleResult = document.querySelector('#title');
+    if(n > n2){
+        titleResult.innerHTML = 'Player 1 Wins!';
+    }
+    else if(n2 > n){
+        titleResult.innerHTML = 'Player 2 Wins!';
+    }
+    else{
+        titleResult.innerHTML = 'Draw...';
     }
 }
---
+++ intermediate_js/extra/modulos.js
@@ -0,0 +1,6 @@
+var mult = [9,18,27,36,45,54,63,72,81,90];
+
+for(i = 0; i < 10; i++){
+    var result = mult[i] % 21;
+    console.log(result);
+}
