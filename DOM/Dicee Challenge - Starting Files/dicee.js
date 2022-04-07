1a53dde2e3501c4ab94d17f7f67159740eab18ab
MASguilherme <guilherme.amsouza@gmail.com>
2022-04-07 16:34:39 -0300

    Dice game

--- DOM/Dicee Challenge - Starting Files/dicee.js
+++ DOM/Dicee Challenge - Starting Files/dicee.js
@@ -1,91 +1,30 @@
 
-var dicesOne = document.querySelector('#diceOne');
-var dicesTwo = document.querySelector('#diceTwo');
 function playDices(){
-    var n = Math.floor(Math.random() * 6 + 1);
-        if(n === 1){
-            dicesOne.innerHTML = `<div class="dice" id="diceOne">
-            <p>Player 1</p>
-            <img class="img1" src="images/dice1.png" alt="dice">
-          </div>`;
+    var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 
-        }
-        if(n === 2){
-            dicesOne.innerHTML = `<div class="dice" id="diceOne">
-        <p>Player 1</p>
-        <img class="img1" src="images/dice2.png" alt="dice">
-      </div>`;
-        }
-        if(n === 3){
-            dicesOne.innerHTML = `<div class="dice" id="diceOne">
-        <p>Player 1</p>
-        <img class="img1" src="images/dice3.png" alt="dice">
-      </div>`;
-        }
-        if(n === 4){
-            dicesOne.innerHTML = `<div class="dice" id="diceOne">
-        <p>Player 1</p>
-        <img class="img1" src="images/dice4.png" alt="dice">
-      </div>`;
-        }
-        if(n === 5){
-            dicesOne.innerHTML = `<div class="dice" id="diceOne">
-        <p>Player 1</p>
-        <img class="img1" src="images/dice5.png" alt="dice">
-      </div>`;
-        }
-        if(n === 6){
-            dicesOne.innerHTML = `<div class="dice" id="diceOne">
-        <p>Player 1</p>
-        <img class="img1" src="images/dice6.png" alt="dice">
-      </div>`;
-        }
-    var n2 = Math.floor(Math.random() * 6 + 1);
-    if(n2 === 1){
-        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
-        <p>Player 2</p>
-        <img class="img2" src="images/dice1.png" alt="dice">
-      </div>`;
-    }
-    if(n2 === 2){
-        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
-        <p>Player 2</p>
-        <img class="img2" src="images/dice2.png" alt="dice">
-      </div>`;
-    }
-    if(n2 === 3){
-        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
-        <p>Player 2</p>
-        <img class="img2" src="images/dice3.png" alt="dice">
-      </div>`;
-    }
-    if(n2 === 4){
-        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
-        <p>Player 2</p>
-        <img class="img2" src="images/dice4.png" alt="dice">
-      </div>`;
-    }
-    if(n2 === 5){
-        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
-        <p>Player 2</p>
-        <img class="img2" src="images/dice5.png" alt="dice">
-      </div>`;
-    }
-    if(n2 === 6){
-        dicesTwo.innerHTML = ` <div class="dice" id="diceTwo">
-        <p>Player 2</p>
-        <img class="img2" src="images/dice6.png" alt="dice">
-      </div>`;
-    }
+    var diceRandomNumber = `images/dice${randomNumber1}.png`;
+    // var imgOne = document.querySelectorAll("img")[0];
+    var imgOne = document.querySelector(".img1");
+    imgOne.setAttribute("src", diceRandomNumber);
 
-    var titleResult = document.querySelector('#title');
-    if(n > n2){
-        titleResult.innerHTML = 'Player 1 Wins!';
+    var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+    var diceRandomNumber2 = `images/dice${randomNumber2}.png`;
+    // var imgTwo = document.querySelectorAll("img")[1];
+    var imgTwo = document.querySelector(".img2");
+    imgTwo.setAttribute("src", diceRandomNumber2);
+
+    var titleResult = document.querySelector("#title");
+    if(randomNumber1 > randomNumber2){
+        titleResult.innerHTML = "Player 1 Wins!";
+
     }
-    else if(n2 > n){
-        titleResult.innerHTML = 'Player 2 Wins!';
+    else if(randomNumber2 > randomNumber1){
+        titleResult.innerHTML = "Player 2 Wins!";
     }
     else{
-        titleResult.innerHTML = 'Draw...';
+        titleResult.innerHTML = "Draw...";
     }
+    
+    
 }
