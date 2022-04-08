68ae2736954d2ea22d96b1d7770ac094d9e3a562
Guilherme Souza <guilherme.amsouza@gmail.com>
2022-04-07 23:59:46 -0300

    js intermediate

--- intermediate_js/extra/selection-sort.js
+++ intermediate_js/extra/selection-sort.js
@@ -1,13 +1,25 @@
-var arr = [8,6,5,10,12];
+var arr = [8, 5, 6, 1, 3, 10, 1];
 
-function selectioSort(){
-    var length = arr.length;
-    var valueMin = arr[0];
-    for(var i = 0; i < length; i++){
-        if(arr[i] < valueMin){
-            valueMin = arr[i];
+function selectioSort() {
+  var length = arr.length;
+  for (var j = 0; j < (length - 1); j++) {
+
+    var valueMin = j;
+
+    for (var i = j; i < (length - 1); i++) {
+
+      if (arr[i] < arr[valueMin]) {
+        arr[valueMin] = arr[i];
+
+        if (arr[j] > arr[valueMin]) {
+
+          var aux = arr[j];
+          arr[j] = arr[valueMin];
+          arr[valueMin] = aux;
         }
+      }
     }
-    console.log(valueMin);
+  }
+  console.log(arr);
 }
 selectioSort();
