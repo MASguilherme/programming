280c10751685bf5b7d2009a1134b382ac1db18ad
MASguilherme <guilherme.amsouza@gmail.com>
2022-04-13 16:42:42 -0300

    DOM and js intermediate

--- DOM/DrumKit/index.js
+++ DOM/DrumKit/index.js
@@ -2,7 +2,12 @@ var drumLength = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < drumLength; i++){
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    alert("i got clicked");
+
+        this.style.color = '#fff';
+
 });
 
 }
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
