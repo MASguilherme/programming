d52b1dcc456eedaa736f47fcff815d4df55b795a
MASguilherme <guilherme.amsouza@gmail.com>
2022-04-13 13:44:05 -0300

    drum kit

--- DOM/DrumKit/index.js
+++ DOM/DrumKit/index.js
@@ -1,3 +1,8 @@
-document.querySelector("#w").addEventListener("click", function (){
-    alert('W-keyword');
+var drumLength = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < drumLength; i++){
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+    alert("i got clicked");
 });
+
+}
