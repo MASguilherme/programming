dfbd76a5459df3d3d07305f888b309689d7ce726
MASguilherme <guilherme.amsouza@gmail.com>
2022-04-14 12:06:07 -0300

    intermediate js

--- intermediate_js/object/ObjectMethodsDotNotation.js
+++ intermediate_js/object/ObjectMethodsDotNotation.js
@@ -4,12 +4,13 @@ function bellBoy(name, age, color){
     this.name = name;
     this.age = age;
     this.color = color;
-    this.takeSuitCase = function takeSuitCase(){
-        console.log("I take your suitCase!")
+    this.takeSuitCase = function () {
+        console.log(`My name is ${name}, and I will take your suitcase.`);
     }
     
 }
 
 
 var boy1 = new bellBoy("Guilherme",21,"brown");
+
 console.log(boy1.takeSuitCase());
