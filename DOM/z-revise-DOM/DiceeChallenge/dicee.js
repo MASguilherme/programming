778108ccc824d52a30684c882eb29cf7bf7c443e
MASguilherme <guilherme.amsouza@gmail.com>
2022-04-19 15:19:07 -0300

    dice challenge jquery update

--- DOM/z-revise-DOM/DiceeChallenge/dicee.js
+++ DOM/z-revise-DOM/DiceeChallenge/dicee.js
@@ -1,24 +1,23 @@
-function playDices(){
-    var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-   
-    var randomDice = `images/dice${randomNumber1}.png`;
-    var diceOne = document.querySelector(".img1");
-    diceOne.setAttribute("src", randomDice);
 
-    var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+$("#play").on("click", function () {
+  var randomNumber1 = Math.floor(Math.random() * 6 + 1);
 
-    var randomDice2 = `images/dice${randomNumber2}.png`;
-    var diceTwo = document.querySelector(".img2");
-    diceTwo.setAttribute("src", randomDice2);
+  var randomDice = `images/dice${randomNumber1}.png`;
+  var diceOne = $(".img1");
+  diceOne.attr("src", randomDice);
 
-    var result = document.querySelector("#title");
-    if(randomNumber1 > randomNumber2){
-        result.innerHTML = "Player 1 Wins!";
-    }
-    else if(randomNumber2 > randomNumber1){
-        result.innerHTML = "Player 2 Wins!";
-    }
-    else{
-        result.innerHTML = "Draw...";
-    }
-}
+  var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+  var randomDice2 = `images/dice${randomNumber2}.png`;
+  var diceTwo = $(".img2");
+  diceTwo.attr("src", randomDice2);
+
+  var result = $("#title");
+  if (randomNumber1 > randomNumber2) {
+    result.text("Player 1 Wins!");
+  } else if (randomNumber2 > randomNumber1) {
+    result.text("Player 2 Wins!");
+  } else {
+    result.text("Draw...");
+  }
+});
