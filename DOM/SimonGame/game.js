11c35b19eef532793f3f94e9ee2f92f1a4bb01be
MASguilherme <guilherme.amsouza@gmail.com>
2022-04-20 16:25:43 -0300

    simon game

--- DOM/SimonGame/game.js
+++ DOM/SimonGame/game.js
@@ -1,16 +1,41 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var userClickedPattern = [];
+
+$(".btn").on("click", function(){
+
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+
+    
+});
+
 
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4)
-
     var randomChosenColour = buttonColours[randomNumber];
-
     gamePattern.push(randomChosenColour);
 
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadein(100);
 
-    var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
     audio.play();
 }
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed")
+    } ,100);
+}
+
