f6e2df8fbbdb9268728362c234ed9f053a680098
Guilherme Souza <guilherme.amsouza@gmail.com>
2022-04-19 22:58:31 -0300

    game jquery

--- DOM/SimonGame/game.js
+++ DOM/SimonGame/game.js
@@ -9,6 +9,8 @@ function nextSequence(){
 
     gamePattern.push(randomChosenColour);
 
-    console.log(gamePattern);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/"+randomChosenColour+".mp3");
+    audio.play();
 }
-nextSequence();
