b824ecfb5f11c19b4123fee68e9390a818da6ae2
Guilherme Souza <guilherme.amsouza@gmail.com>
2022-05-03 07:57:33 -0300

    nodejs

--- nodeJS/serverNodeJs/server.js
+++ nodeJS/serverNodeJs/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const req = require("express/lib/request");
 const app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello World!");
+});
 
 app.listen(3000, function(){
     console.log("My server is running in port 3000!");
