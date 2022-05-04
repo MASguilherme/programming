cf4ad4492ab6d43c8aed5a77b716f4f9589d6c33
Guilherme Souza <guilherme.amsouza@gmail.com>
2022-05-04 00:27:02 -0300

    bmi calculator

--- nodeJS/nodeCalc/calculator.js
+++ nodeJS/nodeCalc/calculator.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const req = require("express/lib/request");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname +"/index.html");
 });
 
 app.post("/", function(req, res){
-    res.send("Thank you for send!");
+    var n1 = Number(req.body.num1);
+    var n2 = Number(req.body.num2);
+    result = n1 + n2;
+
+    res.send("The result of the calculation is: " + result);
+
 });
+
 
 app.listen(3000, function(){
     console.log("My calculator is ready to be building.");
