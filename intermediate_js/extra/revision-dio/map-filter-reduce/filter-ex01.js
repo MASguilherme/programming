3440964c1914c416335018b77c63ab76b5f28189
MASguilherme <guilherme.amsouza@gmail.com>
2022-06-07 13:40:46 -0300

    js filter

---
+++ intermediate_js/extra/revision-dio/map-filter-reduce/filter-ex01.js
@@ -0,0 +1,6 @@
+const fruitBasket = ["Fuji Apple", "Limon", "Green Apple", "Pineapple"];
+
+let fruitFilter = fruitBasket.filter((fruit) => fruit.includes("Apple"));
+console.log(fruitFilter);
+
+// console.log(fruitBasket.filter((fruit) => fruit.includes("Apple")));
