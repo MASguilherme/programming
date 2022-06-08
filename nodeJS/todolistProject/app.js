1fd5829af1d1023c1562b2dcc42bc6ea8ca45070
MASguilherme <guilherme.amsouza@gmail.com>
2022-06-08 16:44:43 -0300

    js project to do list

--- nodeJS/todolistProject/app.js
+++ nodeJS/todolistProject/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+
+app.get("/", (req, res) =>{
+   let today = new Date();
+
+   if(today.getDay() == 6 || today.getDay() == 0 ){
+       res.send("Uhuul, it's the weekend!");
+   }
+   else{
+       res.send("boo! I have to work.");
+   }
+});
+
 app.listen(3000, () =>{
     console.log("Server is running on port 3000!");
 });
