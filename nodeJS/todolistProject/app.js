daaf5c2417c96be623d75d9d07eb17b800560682
MASguilherme <guilherme.amsouza@gmail.com>
2022-06-13 12:01:08 -0300

    ejs

--- nodeJS/todolistProject/app.js
+++ nodeJS/todolistProject/app.js
@@ -1,18 +1,41 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) =>{
-   let today = new Date();
+app.get("/", (req, res) => {
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
 
-   if(today.getDay() == 6 || today.getDay() == 0 ){
-       res.send("Uhuul, it's the weekend!");
-   }
-   else{
-       res.send("boo! I have to work.");
-   }
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wedesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+        console.log("Erro, but why..? " + currentDay);
+  }
+  res.render("list", { kindOfDay: day });
 });
 
-app.listen(3000, () =>{
-    console.log("Server is running on port 3000!");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
 });
