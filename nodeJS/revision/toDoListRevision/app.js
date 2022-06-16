357534ba382cb7348778596f41d80a46a83ddff6
Guilherme Souza <guilherme.amsouza@gmail.com>
2022-06-15 21:16:54 -0300

    to do list revision

---
+++ nodeJS/revision/toDoListRevision/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+
+    res.render("index");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
