06ec7a4840a004f3cb89163c0aa59ba618229736
MASguilherme <guilherme.amsouza@gmail.com>
2022-06-17 14:03:34 -0300

    revision to do list

--- nodeJS/revision/toDoListRevision/app.js
+++ nodeJS/revision/toDoListRevision/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const date = require(__dirname + "/date.js");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -8,9 +8,21 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+let items = [];
 
-    res.render("index");
+app.get("/", (req, res) => {
+    
+    let day = date.getDate();
+
+    res.render("index", {listTitle: day, newItems: items});
+});
+
+app.post("/", (req, res) =>{
+    let newItem = req.body.newItems;
+
+    items.push(newItem);
+
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
