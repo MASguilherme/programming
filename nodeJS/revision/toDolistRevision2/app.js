1820dcff2ae640776437bc17b1e05fced7f2615e
MASguilherme <guilherme.amsouza@gmail.com>
2022-06-28 16:50:47 -0300

    revision

--- nodeJS/revision/toDolistRevision2/app.js
+++ nodeJS/revision/toDolistRevision2/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+
+
+
+
+
+app.get("/", (req, res) =>{
+
+    res.render("index");
+});
+
+
 
 
 app.listen(3000, () =>{
