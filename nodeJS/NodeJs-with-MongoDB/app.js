c8e2f95b04e0fc6de908e266332b3720ac9bdc8c
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-08-31 15:22:44 -0300

    mongoose

--- nodeJS/NodeJs-with-MongoDB/app.js
+++ nodeJS/NodeJs-with-MongoDB/app.js
@@ -1,22 +1,60 @@
+// const mongoose = require("mongoose");
+
+// // Connection URI
+// mongoose.connect("mongodb://localhost:27017/FoodDB", {useNewUrlParser: true});
+
+// const foodSchema = new mongoose.Schema({
+//   _id: Number,
+//   name: String,
+//   price: Number,
+// });
+
+// const Food = mongoose.model("Food", foodSchema);
+
+// const food = new Food({
+//   _id: 2,
+//   name: "2Kg of Bean",
+//   price: 10.99
+// });
+
+// food.save();
+
 const mongoose = require("mongoose");
 
-// Connection URI
-mongoose.connect("mongodb://localhost:27017/FoodDB", {useNewUrlParser: true});
+// connection
+mongoose.connect("mongodb://localhost:27017/PeopleDB", {
+  useNewUrlParser: true,
+});
 
-const foodSchema = new mongoose.Schema({
+// Schema
+const peopleSchema = new mongoose.Schema({
   _id: Number,
   name: String,
-  price: Number,
+  profession: String,
 });
 
+// collection
+const People = mongoose.model("People", peopleSchema);
 
-const Food = mongoose.model("Food", foodSchema);
+// insert data
 
-const food = new Food({
-  _id: 2,
-  name: "2Kg of Bean",
-  price: 10.99
+const marcos = new People({
+  _id: 3,
+  name: "Marcos Pontes",
+  profession: "astronaut"
 });
 
-food.save();
+const vini = new People({
+  _id: 4,
+  name: "Vini Jr",
+  profession: "soccer player"
+});
 
+People.insertMany([marcos, vini], function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log("Peoples save on db")
+  }
+});
