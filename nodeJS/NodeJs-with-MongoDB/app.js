f08d744910d2cdcbba2e7fb0ba39af77a2b454b5
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-08-31 14:35:11 -0300

    mongoose

--- nodeJS/NodeJs-with-MongoDB/app.js
+++ nodeJS/NodeJs-with-MongoDB/app.js
@@ -1,28 +1,22 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
 // Connection URI
-const uri = "mongodb://localhost:27017";
+mongoose.connect("mongodb://localhost:27017/FoodDB", {useNewUrlParser: true});
 
-// Create a new MongoClient
-const client = new MongoClient(uri);
+const foodSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  price: Number,
+});
 
-async function run() {
-  try {
-    const database = client.db("FruitDB");
-    const fruits = database.collection("fruits");
-    // create an array of documents to insert
-    const docs = [
-      { _id:"3", name: "strawberry berry", healthy: true, price: 0.21, stock: 300 },
-      { _id: "4", name: "watermelon melon", healthy: true, price: 7.98, stock: 40 },
-    ];
-    // thiss addi option preventtional documents from being inserted if one fails
-    const options = { ordered: true };
-    const result = await fruits.insertMany(docs, options);
-    console.log(
-      `Connected successfully to server and ${result.insertedCount} documents were inserted`
-    );
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+const Food = mongoose.model("Food", foodSchema);
+
+const food = new Food({
+  _id: 2,
+  name: "2Kg of Bean",
+  price: 10.99
+});
+
+food.save();
+
