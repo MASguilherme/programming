4e184a6f6892518363ec7c65af1ff87247661a54
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-09-16 16:22:47 -0300

    RESTfull

---
+++ nodeJS/RESTful-API/app.js
@@ -0,0 +1,24 @@
+const express = require('express'); 
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+mongoose.connect("mongodb://localhost:27017/WikiDB");
+
+// Schema
+const articleSchema = new mongoose.Schema({
+    title: String, 
+    content: String,
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.listen(3000, (req, res) =>{
+    console.log("Server is running on port 3000!");
+});
