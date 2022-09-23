26c5b97a853c99ba8c4ece533857b99ccfb582dc
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-09-23 16:53:03 -0300

    security module

--- nodeJS/NodeJs-with-MongoDB/Secrets/app.js
+++ nodeJS/NodeJs-with-MongoDB/Secrets/app.js
@@ -1,1 +1,65 @@
-//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+const userSchema = {
+    email: String,
+    password: String
+}
+const User = new mongoose.model("User", userSchema);
+
+app.route("/")
+.get((req, res) =>{
+    res.render("home");
+});
+
+app.route("/login")
+.get((req, res) =>{
+    res.render("login");
+}).post((req, res) =>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            return err;
+        }else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.route("/register")
+.get((req, res) =>{
+    res.render('register');
+})
+.post((req, res) =>{
+     const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+     });
+
+     newUser.save((err) =>{
+        if(err){
+            return err;
+        }if(foundUser){
+            if(foundUser.password === password){
+                res.render('secrets');
+            }
+        }
+     })
+});
+
+app.listen(3000, (req, res) =>{
+
+    console.log("server is running on port 3000!");
+});
