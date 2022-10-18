995649d53e53f139bd871898deb738ea03a40fb7
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-10-18 11:48:17 -0300

    keeper React project

---
+++ ReactJs/KeeperProject/keeper/src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <section class="note-section">
+      <div className="note-wrap">
+        <div class="note">
+          <h3>Note Title</h3>
+          <p> Note paragraphs</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Note;
