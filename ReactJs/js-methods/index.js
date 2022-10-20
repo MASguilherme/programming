5e9e9f26653d5baaa3731b2e90e70ae19d82097f
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-10-20 16:58:26 -0300

    js theory

---
+++ ReactJs/js-methods/index.js
@@ -0,0 +1,14 @@
+let arr = [1, 2, 3, 5, 6];
+
+
+const newNumbers = arr.map(function(x){
+    return x *2;
+});
+
+// var newNumbers = [];
+
+// arr.forEach(function(x){
+//     newNumbers.push(x * 2);
+// });
+
+console.log(newNumbers);
