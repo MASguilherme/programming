b4f08b2f28934509c733673945c6258b493ddf90
Guilherme <guilherme.amsouza@gmail.com>
2022-11-07 14:10:09 -0300

    crud react com typescript

---
+++ ReactJs/React-Mui-Ts/react-typescript/README.md
@@ -0,0 +1,1 @@
+# Sistema de cadastros básicos - CRUD
---
+++ ReactJs/React-Mui-Ts/react-typescript/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className="App">
+        <h1>Hello Typescript</h1>
+    </div>
+  );
+}
