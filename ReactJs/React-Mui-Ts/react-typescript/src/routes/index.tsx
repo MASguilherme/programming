e3ac5f545ad98062708220cfcd8de27bea53540e
Guilherme <guilherme.amsouza@gmail.com>
2022-11-07 16:50:05 -0300

    feat: adicionando tema claro e provider

--- ReactJs/React-Mui-Ts/react-typescript/src/routes/index.tsx
+++ ReactJs/React-Mui-Ts/react-typescript/src/routes/index.tsx
@@ -1,9 +1,17 @@
+import Button from "@mui/material/Button";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="pagina-inicial" element={<p>Página inicial</p>} />
+      <Route
+        path="pagina-inicial"
+        element={
+          <Button color={"primary"} variant={"contained"}>
+            Enviar
+          </Button>
+        }
+      />
 
       <Route path="*" element={<Navigate to="pagina-inicial" />} />
     </Routes>
