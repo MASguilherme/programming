66325287d85552cbe3c7c2ac49ec152f644b07f4
Guilherme <guilherme.amsouza@gmail.com>
2022-11-09 21:18:48 -0300

    react basic

--- ReactJs/Basic/basic/src/app/routes/index.tsx
+++ ReactJs/Basic/basic/src/app/routes/index.tsx
@@ -1,11 +1,15 @@
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+
+import {BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard } from "../pages";
 
 export const AppRoutes = () =>{
 
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<h1>Olá mundo</h1>} />
+            <Route path="/pagina-principal" element={<Dashboard />} />
+            <Route path="/produtos" element={<h1>Olá mundo, barra page</h1>} />
+            <Route path="*" element={<Navigate to="/pagina-principal"/>} />
         </Routes>
     </BrowserRouter>
     );
