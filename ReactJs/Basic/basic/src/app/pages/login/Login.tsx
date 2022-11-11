d052920856b05a6fb7b959033ecc692320ff7cc1
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-11-11 10:55:49 -0300

    use state react

--- ReactJs/Basic/basic/src/app/pages/login/Login.tsx
+++ ReactJs/Basic/basic/src/app/pages/login/Login.tsx
@@ -1,15 +1,34 @@
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-export const Login = () =>{
-    const navigate = useNavigate();
-    const handleClick = () =>{
-        navigate("/pagina-inicial");    
-    }
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEntrar = () => {
+    console.log(email +" "+ password);
+  };
 
-    return(
-        <div>
-            <p>Login</p>
-            <button onClick={handleClick}>Pagina inicial</button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <form>
+        <label>
+          <span>E-mail:</span>
+          <input
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleEntrar}>
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+};
