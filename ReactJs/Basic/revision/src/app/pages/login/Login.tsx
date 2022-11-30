d8821f4ac4d7ca9edf060963f61b0c04c7514fb7
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-11-30 17:08:06 -0300

    revisão useEffect

--- ReactJs/Basic/revision/src/app/pages/login/Login.tsx
+++ ReactJs/Basic/revision/src/app/pages/login/Login.tsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Login = () => {
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (window.confirm("Você é Homem?")) {
+      console.log("Você é Homem!");
+    } else {
+      console.log("Você não é homem.");
+    }
+  }, []);
+
+  useEffect(()=>{
+    console.log("E-mail: " + email);
+  },[email]);
+
+  useEffect(()=>{
+    console.log("Senha: " + password);
+  },[password]);
+
   const handleLogin = () => {
-    console.log("E-mail: " + email + " Senha: "+ password);
+    console.log("Dados enviados: E-mail: " + email + " Senha: " + password);
   };
 
   return (
@@ -13,17 +29,19 @@ export const Login = () => {
       <form>
         <label>
           <span>E-mail: </span>
-          <input
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
+          <input value={email} onChange={e => setEmail(e.target.value)} />
         </label>
         <label>
           <span>Senha: </span>
-          <input type="password" 
-          value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </label>
-        <button type="button" onClick={handleLogin}>Entrar</button>
+        <button type="button" onClick={handleLogin}>
+          Entrar
+        </button>
       </form>
     </div>
   );
