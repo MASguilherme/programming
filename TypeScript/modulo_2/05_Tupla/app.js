88fdb9629badf75cb4d1e6a652fc0d1b74985e2c
Guilherme <guilherme.amsouza@gmail.com>
2022-12-02 21:24:45 -0300

    tupla typescript

--- TypeScript/modulo_2/05_Tupla/app.js
+++ TypeScript/modulo_2/05_Tupla/app.js
@@ -1,11 +1,28 @@
 "use strict";
 // EX 01 - Dados pessoais
 let pessoa;
-pessoa = ['Guilherme', 21, false];
-console.log(pessoa);
+pessoa = ["Guilherme", 21, false];
+// console.log(pessoa);
 // Ex 02 - Acessando o valor de tuplas
+let pessoa1;
+pessoa1 = ['Guilherme', 21, false];
+// console.log(...pessoa1);
 // ex 03 - Outra forma de usar tuplas (com labels)
+let aluno;
+aluno = ['José', 10, true];
+// console.log(`Aluno ${aluno[0]} tirou a nota ${aluno[1]}. Aprovação: ${aluno[2]}`);
 // Ex 04 - Usando Tuplas com Spread Operation
+let listaFrutas = ['🍐', '🍓', '🍈', '🍇', '🍌'];
+// console.log(...listaFrutas);
 // Ex 05 - Lista Heterogênea de Tupla
+let infoListaFruta = [10, false, ...listaFrutas];
+// console.log(infoListaFruta);
 // Ex 06 - Uso da função com tuplas
-// Ex 07 - Labeled Tuples com Spread Operator numa função
+function ListaNomes(nomes, idades) {
+    return [nomes, idades];
+}
+let infoListaNomes = ListaNomes(['Guilherme', 'Saulo'], [21, 17]);
+function CriarPessoa(...pessoa) {
+    return [...pessoa];
+}
+console.log(CriarPessoa('Guilherme', 'Souza'));
--- TypeScript/modulo_2/05_Tupla/app.ts
+++ TypeScript/modulo_2/05_Tupla/app.ts
@@ -1,18 +1,42 @@
 // EX 01 - Dados pessoais
 let pessoa: [string, number, boolean];
-
-pessoa = ['Guilherme',21, false];
-console.log(pessoa);
-
+pessoa = ["Guilherme", 21, false];
+// console.log(pessoa);
 
 // Ex 02 - Acessando o valor de tuplas
+let pessoa1: [string, number, boolean ];
+pessoa1 = ['Guilherme', 21, false];
+// console.log(...pessoa1);
 
 // ex 03 - Outra forma de usar tuplas (com labels)
+let aluno: [nome: string, nota: number, aprovado: boolean];
+aluno = ['José', 10, true];
+// console.log(`Aluno ${aluno[0]} tirou a nota ${aluno[1]}. Aprovação: ${aluno[2]}`);
 
 // Ex 04 - Usando Tuplas com Spread Operation
+let listaFrutas: [string, ...string[]] = ['🍐', '🍓', '🍈', '🍇', '🍌'];
+// console.log(...listaFrutas);
 
 // Ex 05 - Lista Heterogênea de Tupla
+let infoListaFruta: [number, boolean, ...string[]] = [10, false, ...listaFrutas];
+// console.log(infoListaFruta);
 
 // Ex 06 - Uso da função com tuplas
+function ListaNomes(nomes: string[], idades: number[]){
+    return [nomes, idades];
+}
+let infoListaNomes = ListaNomes(['Guilherme', 'Saulo'], [21, 17]);
+// console.log(...infoListaNomes);
 
 // Ex 07 - Labeled Tuples com Spread Operator numa função
+
+type Pessoa =
+            | [firstName: string, lastName: string]
+            | [firstName: string, middleName: string, lastName: string]
+            | [firstName: string, middleName:string, lastName:string, age: number];
+
+function CriarPessoa(...pessoa: Pessoa){
+    return [...pessoa];
+}
+
+console.log(CriarPessoa('Guilherme', 'Souza'));
