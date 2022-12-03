0760d62355a94ad96342e9a1923e431fdcb30af8
Guilherme <guilherme.amsouza@gmail.com>
2022-12-02 21:51:32 -0300

    useMemo react

--- ReactJs/Basic/revision/src/app/pages/login/Login.tsx
+++ ReactJs/Basic/revision/src/app/pages/login/Login.tsx
@@ -1,24 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  useEffect(() => {
-    if (window.confirm("Você é Homem?")) {
-      console.log("Você é Homem!");
-    } else {
-      console.log("Você não é homem.");
-    }
-  }, []);
-
-  useEffect(()=>{
-    console.log("E-mail: " + email);
-  },[email]);
-
-  useEffect(()=>{
-    console.log("Senha: " + password);
-  },[password]);
+  const emailLength = useMemo(()=>{
+    console.log('executou');
+    return email.length;
+  }, [email]);
 
   const handleLogin = () => {
     console.log("Dados enviados: E-mail: " + email + " Senha: " + password);
@@ -26,17 +15,18 @@ export const Login = () => {
 
   return (
     <div>
+      <p>Quantidade de caracteres do e-mail: {emailLength}</p>
       <form>
         <label>
           <span>E-mail: </span>
-          <input value={email} onChange={e => setEmail(e.target.value)} />
+          <input value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
         <label>
           <span>Senha: </span>
           <input
             type="password"
             value={password}
-            onChange={e => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </label>
         <button type="button" onClick={handleLogin}>
