e1c487534b67ca3c506185e9155ca1338c644d40
Guilherme <guilherme.amsouza@gmail.com>
2022-12-08 22:02:25 -0300

    json server

--- ReactJs/Basic/reactrevision/src/app/pages/dashboard/Dashboard.tsx
+++ ReactJs/Basic/reactrevision/src/app/pages/dashboard/Dashboard.tsx
@@ -33,14 +33,42 @@ export const Dashboard = () => {
       }
     }, []);
 
+  const handleChecklist = () => {};
+
   return (
     <div>
       <h1>Dashboard</h1>
       <h2>Lista</h2>
+      <p>
+        Itens selecionados:{" "}
+        {lista.filter((listItem) => listItem.isSelected).length}
+      </p>
       <input type="text" onKeyDown={handleInputKeyDown} />
       <ul>
         {lista.map((listItem, index) => {
-          return <li key={index}>{listItem.item}</li>;
+          return (
+            <li key={index}>
+              <input
+                type="checkbox"
+                checked={listItem.isSelected}
+                onChange={() => {
+                  setLista((oldLista) => {
+                    return oldLista.map((oldListItem) => {
+                      const newIsSelected =
+                        oldListItem.item === listItem.item
+                          ? !oldListItem.isSelected
+                          : oldListItem.isSelected;
+                      return {
+                        ...oldListItem,
+                        isSelected: newIsSelected,
+                      };
+                    });
+                  });
+                }}
+              />
+              {listItem.item}
+            </li>
+          );
         })}
       </ul>
     </div>
