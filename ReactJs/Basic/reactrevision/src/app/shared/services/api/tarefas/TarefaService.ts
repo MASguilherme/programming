35941fa2069652f8e8c19fc1660223b9316b098f
Guilherme <guilherme.amsouza@gmail.com>
2022-12-10 16:12:30 -0300

    service react

--- ReactJs/Basic/reactrevision/src/app/shared/services/api/tarefas/TarefaService.ts
+++ ReactJs/Basic/reactrevision/src/app/shared/services/api/tarefas/TarefaService.ts
@@ -12,7 +12,7 @@ const getAll = async (): Promise<ITarefa[] | ApiException> => {
     const { data } = await Api().get("/tarefas");
     return data;
   } catch (error: any) {
-    return new ApiException(error.message || "Erro ao consultar API");
+    return new ApiException(error.message || "Erro ao consultar os registros");
   }
 };
 
@@ -21,14 +21,36 @@ const getById = async (id: number): Promise<ITarefa | ApiException> => {
     const { data } = await Api().get(`/tarefas/${id}`);
     return data;
   } catch (error: any) {
-    return new ApiException(error.message || "Erro ao consultar API");
+    return new ApiException(error.message || "Erro ao consultar o registro.");
   }
 };
-const create = () => {};
+const create = async (dataToCreate: Omit<ITarefa, 'id'>): Promise<ITarefa | ApiException> => {
+  try{
+    const {data} = await Api().post('/tarefas', dataToCreate);
+    return data;
+  }catch(error: any){
+    return new ApiException(error.message || 'Erro ao criar o registro.');
+  }
+};
 
-const updateById = () => {};
 
-const deleteById = () => {};
+const updateById = async (id: number, dataToUpdate: ITarefa): Promise<ITarefa | ApiException> => {
+  try {
+    const { data } = await Api().put(`tarefas/${id}`, dataToUpdate);
+    return data;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao atualizar o registro");
+  }
+};
+
+const deleteById = async (id: number): Promise<undefined | ApiException> => {
+  try {
+    await Api().get(`tarefas/${id}`);
+    return undefined;
+  } catch (error: any) {
+    return new ApiException(error.message || "Erro ao apagar o registro");
+  }
+};
 
 export const TarefaService = {
   getAll,
