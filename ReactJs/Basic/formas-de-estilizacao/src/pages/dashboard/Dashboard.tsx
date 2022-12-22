6ec3e1fcad050b8f68facb4f1d56b502cdcdc2c9
Guilherme Souza <guilherme_souza@mddweb.com.br>
2022-12-22 12:06:02 -0300

    finalizando react com css

--- ReactJs/Basic/formas-de-estilizacao/src/pages/dashboard/Dashboard.tsx
+++ ReactJs/Basic/formas-de-estilizacao/src/pages/dashboard/Dashboard.tsx
@@ -1,5 +1,7 @@
 import { createUseStyles } from "react-jss";
 
+import styled from 'styled-components';
+
 const useClasses = createUseStyles({
     paragraph: {
         backgroundColor: 'black',
@@ -12,12 +14,23 @@ const useClasses = createUseStyles({
     }
 });
 
+const Paragraph = styled.p`
+    background-color: orange;
+    color: red;
+    font-size: 3rem;
+
+    :hover{
+        background-color: black;
+    }
+`;    
+
 export const Dashboard = () => {
 
     const classes = useClasses();
   return (
     <div>
       <h1 className={classes.paragraph}>Dashboard</h1>
+      <Paragraph>Teste</Paragraph>
     </div>
   );
 };
