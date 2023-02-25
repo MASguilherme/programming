c80f77ad9f4a3d95915bd39e83a73311675927b6
Guilherme <guilherme.amsouza@gmail.com>
2023-02-25 18:08:25 -0300

    sql aula revisão

--- sql/revisaosql/aula04.sql
+++ sql/revisaosql/aula04.sql
@@ -1,8 +1,3 @@
-CREATE TABLE cliente(
-	id_cliente_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    s_nome_cliente VARCHAR(30) NOT NULL,
-    s_cpf_cliente VARCHAR(11) NOT NULL,
-    d_nasc_cliente DATETIME
-);
+ALTER TABLE cliente ADD i_idade_cliente INT NOT NULL;
 
-ALTER TABLE cliente ADD(i_tipo_cliente INT NOT NULL);
+ALTER TABLE cliente MODIFY i_idade_cliente TINYINT;
