bf1e2965f80b23813023345236e5e20faf4f23c2
Guilherme <guilherme.amsouza@gmail.com>
2023-02-27 20:26:42 -0300

    revisão sql

---
+++ sql/revisaosql/update.sql
@@ -0,0 +1,9 @@
+UPDATE cliente SET s_nome_cliente='Guilherme' WHERE id_cliente_cliente = 1;
+
+UPDATE cliente SET s_cpf_cliente = '01234567891' WHERE id_cliente_cliente > 0;
+
+DELETE from cliente WHERE id_cliente_cliente > 0;
+
+INSERT INTO cliente SELECT * FROM cliente_dois;
+
+SELECT * FROM cliente;
